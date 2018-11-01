a7e2fd28d2127ea71cf50a2dc78c4b7e152f96c2
Bengt Sennblad <bengt.sennblad@scilfelab.se>
2018-11-01 13:11:42 +0100

    generalized prohject name

--- CONDAME.sh
+++ CONDAME.sh
@@ -7,24 +7,26 @@ conda config --add channels conda-forge
 conda config --add channels bioconda
 conda config --add channels defaults
 
+PROJ="j_frisen_1602_tmp" 
+
 USROPT=""
 if [[ -n $1 ]]; then
     USROPT=$1
 fi
 
-exists=`conda info --envs|grep -v "#"|awk '{if($1=="j_frisen_1602")print 0}'`
+exists=`conda info --envs|grep -v "#"|awk '{if($1=="$PROJ")print 0}'`
 if test "$exists" = "0"; then
-    echo "Updating conda environment j_frisen_1602";
-    conda install $USROPT -n j_frisen_1602 --file requirements.txt;
+    echo "Updating conda environment $PROJ";
+    conda install $USROPT -n $PROJ --file requirements.txt;
 else
-    echo "Now creating conda environment j_frisen_1602";
-    conda create $USROPT -n j_frisen_1602  --file requirements.txt;
+    echo "Now creating conda environment $PROJ";
+    conda create $USROPT -n $PROJ  --file requirements.txt;
 fi
 gatk-register /media/box2/Experiments/Joanna/bin/downloads/GenomeAnalysisTK-3.6.tar.bz2
 echo "To start conda environment type:"
-echo "source activate j_frisen_1602"
+echo "source activate $PROJ"
 echo "To stop conda environment type:"
-echo "source deactivate [j_frisen_1602]"
+echo "source deactivate [$PROJ]"
 # To remove conda environment from system: conda env remove -n
-# j_frisen_1602 (convenient if you need to add or update the packages
+# $PROJ (convenient if you need to add or update the packages
 # installed by conda).
